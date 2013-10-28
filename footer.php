d89ade4ba7d7a90f2ba331e78add0f6b8d05f141
Lewis Carey <lewismcarey@ED-209.local>
2013-10-28 15:31:04 +0000

    Added template directory to jquery fallback

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
 		</footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+		<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/inc/jquery-1.9.1.min.js"><\/script>')</script>
 
         <?php wp_footer(); ?>
 
