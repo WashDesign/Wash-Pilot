b676835bc5aa93fc14f7735428c6f9313a6c0bd9
Lewis Carey <lewismcarey@ED-209.local>
2013-10-28 15:24:33 +0000

    Removed hardcoded scripts and styles
    Likewise

--- footer.php
+++ footer.php
@@ -26,8 +26,8 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="assets/js/plugins.js"></script>
-        <script src="assets/js/main.js"></script>
+
+        <?php wp_footer(); ?>
 
         <?php //Google Analytics: change UA-XXXXX-X to be your site's ID. ?>
         <script>
